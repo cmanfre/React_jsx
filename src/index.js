b3926ffd2b385c515e33c99cc9f50b9e09f0ff4c
Carly Manfre <cmanfre@mail.usf.edu>
2019-03-31 21:36:06 -0400

    first commit

---
+++ src/index.js
@@ -0,0 +1,28 @@
+// Import the React and ReactDOM libraries
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// Create a react component
+const App = () => {
+	const buttonText = 'Click Me!';
+	const labelText = 'Enter name:';
+
+	return (
+		<div>
+			<label className="label" htmlFor="name">
+				{labelText}
+			</label>
+			<input id="name" type="text" />
+			<button style={{ backgroundColor: 'blue', color: 'white' }}>
+				{buttonText}
+			</button>
+		</div>
+	)
+};
+
+
+// Take the react component and show it on 
+ReactDOM.render(
+	<App />,
+	document.querySelector('#root')
+	);
